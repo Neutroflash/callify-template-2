e4f94cc6ec498232ec655e1ef2b2ea2c3efa011e
Neutroflash <neutroflashp@hotmail.com>
2025-02-27 16:48:24 -0500

    add-config

--- callify/callify/wsgi.py
+++ callify/callify/wsgi.py
@@ -1,10 +1,6 @@
 import os
-import sys
-
-sys.path.append("/app/callify")  # Ajusta la ruta según cómo Railway monta tu código
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "callify.settings")
-
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "callify.callify.settings")
 
 application = get_wsgi_application()
